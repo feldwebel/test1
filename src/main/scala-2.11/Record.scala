1a1682dcde777bab5a4d4b033b2aa9fdcf1d816e
Dan Tereschenko <d.tereschenko@paymantix.com>
2016-04-11 20:01:14 +0300

    first draft

--- src/main/scala-2.11/Record.scala
+++ src/main/scala-2.11/Record.scala
@@ -1,9 +1,9 @@
 /**
   * Created by user on 08.04.16.
   */
-class Record (name:String, salary: Int, department: String) {
+class Record (val id: Int, val name:String, val salary: Int, val department: String) {
 
-  val id = java.util.UUID.randomUUID.toString
-
-  override def toString(): String = (id + ": " + name + " " + salary + department)
+  override def toString(): String = {
+    id + ": " + name + " " + salary + " " + department + "\n"
+  }
 }
--- src/main/scala-2.11/main.scala
+++ src/main/scala-2.11/main.scala
@@ -2,22 +2,64 @@
   * Created by user on 08.04.16.
   */
 import scala.io.StdIn.readLine
-import scala.collection.mutable.ArrayBuffer
 
 object main extends App {
 
-  var records = ArrayBuffer.empty[Record]
+  val workData = new DataContainer()
+  var loop = true
 
-  main()
+  while (loop) {
+    showMenu()
 
-  def main(): Unit =
-  {
-    println("fio:")
-    val fio = readLine()
-    val salary : Int = readLine().toInt
-    val department = readLine()
+    readLine().toInt match {
+      case 0 => loop = false
+      case 1 => addRecord()
+      case 2 => showUsers()
+      case 3 => totalSalary()
+      case 4 => totalSalaryByDepartment()
+      case 5 => averageSalaryByDepartment()
+      case 6 => findUser()
+      case _ =>
+    }
+  }
 
-    records += new Record(fio, salary, department)
-    println(records.lastOption.toString())
+  private def addRecord(): Unit = {
+    val fio = readLine("name: ")
+    val salary : Int = readLine("salary: ").toInt
+    val department = readLine("department: ")
+
+    workData.add(fio, salary, department)
+  }
+
+  private def showUsers(): Unit = {
+    workData.showAll()
+  }
+
+  private def totalSalary(): Unit = {
+    println(workData.totalSum())
+  }
+
+  private def totalSalaryByDepartment(): Unit = {
+    println(workData.totalSum(readLine("Enter department: ")))
+  }
+
+  private def averageSalaryByDepartment(): Unit = {
+    println(workData.averageSum(readLine("Enter department: ")))
+  }
+
+  private def findUser(): Unit = {
+    println(workData.findUser(readLine("Enter name: ")))
+  }
+
+  private def showMenu() : Unit = {
+    print("\u001b[H\u001b[2J")
+    println("Choose command:")
+    println("1 - add user")
+    println("2 - show users")
+    println("3 - total salary")
+    println("4 - total salary by department")
+    println("5 - average salary by department")
+    println("6 - find user by name")
+    println("0 - exit")
   }
 }
